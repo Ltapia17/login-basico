46ac2c5c3d6017c6b4f967a02b013bba11d087a8
Ltapia17 <48846311+Ltapia17@users.noreply.github.com>
2019-09-26 03:27:28 -0300

    Update init.js

--- src/firebase/init.js
+++ src/firebase/init.js
@@ -2,13 +2,13 @@ import firebase from 'firebase'
 import firestore from 'firebase/firestore'
 
   const firebaseConfig = {
-    apiKey: "AIzaSyBzQ_iGj9t4bpmshFxRlvsVLdGa88wbn_Q",
-    authDomain: "vuejs-91219.firebaseapp.com",
-    databaseURL: "https://vuejs-91219.firebaseio.com",
-    projectId: "vuejs-91219",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
     storageBucket: "",
-    messagingSenderId: "963217498345",
-    appId: "1:963217498345:web:12a8cce7ac54ac39bc5078"
+    messagingSenderId: "",
+    appId: ""
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
